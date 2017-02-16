4b1b2221dc2163b1ca007fe4d8081aec3c919762
Josh Terry <joshterry55@gmail.com>
2017-02-15 17:44:27 -0700

    image placeholder

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 const App = ({ children }) => (
-  <div>
-    <div>
-      test
+  <div className='row'>
+
+    <div className="col s12 m6 offset-m3" style={{height: '500px'}}>
+      <div style={{
+          backgroundImage: `url('http://res.cloudinary.com/dk2bj79p0/image/upload/v1487205269/FullSizeRender_oyhivj.jpg')`,
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center center',
+          width: '100%',
+          height: '100%',
+          maxWidth: '500px',
+          display: 'block',
+          borderRadius: '10px',
+          margin: '0px auto',
+          position: 'relative',
+          opacity: '0.65',
+          zIndex: '2',
+          cursor: 'pointer',
+        }}>
+      </div>
+    </div>
+    <div className='col s12 center' style={{marginLeft: '12px'}}>
+      <h1>Coming Soon!</h1>
     </div>
     { children }
   </div>
